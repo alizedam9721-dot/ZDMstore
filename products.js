e09cfbd4c2cedf46eff254193c42464d4294bd4d
alizedam9721-dot <alizedam9721@gmail.com>
2026-07-02 21:25:48 +0100

    Create products.js

---
+++ products.js
@@ -0,0 +1,7 @@
+var myProducts = [
+    {
+        name: "ساعة ذكية Smart Watch",
+        price: "4500 دج",
+        image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEijKxiO9k4NGZC29Tx-kTJWOCv18J3gQl7shoKpna67pIhd4MRbMKTwJHyb1lIeqmI3jphxDwIXB6uoXZDop-OrvMTabSWIJCAflHfk6ICtCxEEE5eaivFxqQAhe576Bzk8iZ8hQl8BoiuT9s_JLqPsHbUNo7hP3HEuMCvjThC7UfoCW5oUoSX8D6ZufuA/s662/LS20260702140733.png"
+    }
+];
