8d2920dc8434323e48ba60b813e1874bfe34092e
alizedam9721-dot <alizedam9721@gmail.com>
2026-07-02 23:05:34 +0100

    Update products.js

--- products.js
+++ products.js
@@ -1,23 +1,20 @@
-var products = [
-    {
-        "id": 1,
-        "اسم": "مجموعة أساور ذهبية راقية (Acier Inoxydable)",
-        "سعر": 2500,
-        "صورة": "https://raw.githubusercontent.com/alizedam9721-dot/ZDMstore/main/images/Bracelets.jpg.heic",
-        "وصف": "مجموعة أساور مصنوعة من الفولاذ المقاوم للصدأ بجودة عالية تناسب كل المناسبات."
-    },
-    {
-        "id": 2,
-        "اسم": "أحمر شفاه كبسولة 12 لون Mini Bag",
-        "سعر": 1800,
-        "صورة": "https://raw.githubusercontent.com/alizedam9721-dot/ZDMstore/main/images/lipstick.jpg.jpg",
-        "وصف": "حقيبة صغيرة تحتوي على 12 لون مميز من أحمر الشفاه على شكل كبسولات مات."
-    },
-    {
-        "id": 3,
-        "اسم": "طقم لانجري نسائي راقي - 9 قطع متنوعة",
-        "سعر": 4500,
-        "صورة": "https://raw.githubusercontent.com/alizedam9721-dot/ZDMstore/main/images/lingerie1.jpg.jpg",
-        "وصف": "مجموعة حصرية من 9 قطع ملابس شبكية جذابة ومختلفة مرنة وتناسب جميع المقاسات بجودة ممتازة."
-    }
+window.products = [
+  {
+    "id": 1,
+    "name": "مجموعة أساور ذهبية راقية (Acier Inoxydable)",
+    "price": "2500 دج",
+    "image": "images/Bracelets.jpg.heic"
+  },
+  {
+    "id": 2,
+    "name": "أحمر شفاه كبسولة 12 لون Mini Bag",
+    "price": "1800 دج",
+    "image": "images/lipstick.jpg.jpg"
+  },
+  {
+    "id": 3,
+    "name": "طقم لانجري نسائي راقي - 9 قطع متنوعة",
+    "price": "4500 دج",
+    "image": "images/lingerie1.jpg.jpg"
+  }
 ];
