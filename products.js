1625a552fd92e3d78ce2f147d5bd867f8fd61803
alizedam9721-dot <alizedam9721@gmail.com>
2026-07-02 22:45:19 +0100

    Update products.js

--- products.js
+++ products.js
@@ -1,17 +1,23 @@
 const products = [
   {
-    "name": "مجموعة أساور ذهبية راقية (Acier Inoxydable)",
-    "price": "2500 دج",
-    "image": "images/Bracelets.jpg.heic"
+    id: 1,
+    name: "مجموعة أساور ذهبية راقية (Acier Inoxydable)",
+    price: 2500,
+    image: "https://raw.githubusercontent.com/alizedam9721-dot/ZDMstore/main/images/Bracelets.jpg.heic",
+    description: "مجموعة أساور مصنوعة من الفولاذ المقاوم للصدأ بجودة عالية تناسب كل المناسبات."
   },
   {
-    "name": "أحمر شفاه كبسولة 12 لون Mini Bag",
-    "price": "1800 دج",
-    "image": "images/lipstick.jpg.jpg"
+    id: 2,
+    name: "أحمر شفاه كبسولة 12 لون Mini Bag",
+    "price": 1800,
+    image: "https://raw.githubusercontent.com/alizedam9721-dot/ZDMstore/main/images/lipstick.jpg.jpg",
+    description: "حقيبة صغيرة تحتوي على 12 لون مميز من أحمر الشفاه على شكل كبسولات مات."
   },
   {
-    "name": "طقم لانجري نسائي راقي - 9 قطع متنوعة",
-    "price": "4500 دج",
-    "image": "images/lingerie1.jpg.jpg"
+    id: 3,
+    name: "طقم لانجري نسائي راقي - 9 قطع متنوعة",
+    price: 4500,
+    image: "https://raw.githubusercontent.com/alizedam9721-dot/ZDMstore/main/images/lingerie1.jpg.jpg",
+    description: "مجموعة حصرية من 9 قطع ملابس شبكية جذابة ومختلفة مرنة وتناسب جميع المقاسات بجودة ممتازة."
   }
 ];
