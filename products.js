8405a27d136774b3911d79b8bfb144433820d325
alizedam9721-dot <alizedam9721@gmail.com>
2026-07-02 22:31:22 +0100

    Update products.js

--- products.js
+++ products.js
@@ -1,17 +1,17 @@
 var products = [
-    {
-        name: "مجموعة أساور ذهبية راقية (Acier Inoxydable)",
-        price: "2500 دج",
-        image: "images/Bracelets.jpg.heic"
-    },
-    {
-        name: "أحمر شفاه كبسولة 12 لون Mini Bag",
-        price: "1800 دج",
-        image: "images/lipstick.jpg.jpg"
-    },
-    {
-        name: "طقم لانجري نسائي راقي - 9 قطع متنوعة",
-        price: "4500 دج",
-        image: "images/lingerie1.jpg.jpg"
-    }
+  {
+    "name": "مجموعة أساور ذهبية راقية (Acier Inoxydable)",
+    "price": "2500 دج",
+    "image": "images/Bracelets.jpg.heic"
+  },
+  {
+    "name": "أحمر شفاه كبسولة 12 لون Mini Bag",
+    "price": "1800 دج",
+    "image": "images/lipstick.jpg.jpg"
+  },
+  {
+    "name": "طقم لانجري نسائي راقي - 9 قطع متنوعة",
+    "price": "4500 دج",
+    "image": "images/lingerie1.jpg.jpg"
+  }
 ];
