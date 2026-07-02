7855ccba24ad25a00f9e2df9e12d44efef46e662
alizedam9721-dot <alizedam9721@gmail.com>
2026-07-02 21:51:31 +0100

    Update products.js

--- products.js
+++ products.js
@@ -1,7 +1,26 @@
-var myProducts = [
+const products = [
     {
-        name: "ساعة ذكية Smart Watch",
-        price: "4500 دج",
-        image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEijKxiO9k4NGZC29Tx-kTJWOCv18J3gQl7shoKpna67pIhd4MRbMKTwJHyb1lIeqmI3jphxDwIXB6uoXZDop-OrvMTabSWIJCAflHfk6ICtCxEEE5eaivFxqQAhe576Bzk8iZ8hQl8BoiuT9s_JLqPsHbUNo7hP3HEuMCvjThC7UfoCW5oUoSX8D6ZufuA/s662/LS20260702140733.png"
+        id: 1,
+        name: "مجموعة أساور ذهبية راقية (Acier Inoxydable)",
+        price: 2500, // السعر بالدينار الجزائري (مثال: 250 ألف)
+        originalPrice: 3500,
+        image: "https://raw.githubusercontent.com/alizedam9721/zdmstore/main/images/bracelets.jpg",
+        description: "مجموعة أساور (أنورب) من الفولاذ المقاوم للصدأ بجودة عالية، تصميم عصري ولامع يناسب كل المناسبات."
+    },
+    {
+        id: 2,
+        name: "أحمر شفاه كبسولة 12 لون Mini Bag",
+        price: 1800, // السعر بالدينار (مثال: 180 ألف)
+        originalPrice: 2400,
+        image: "https://raw.githubusercontent.com/alizedam9721/zdmstore/main/images/lipstick.jpg",
+        description: "حقيبة صغيرة تحتوي على 12 لون مميز من أحمر الشفاه على شكل كبسولات مات وناعمة، سهلة الحمل في الحقيبة اليومية."
+    },
+    {
+        id: 3,
+        name: "طقم لانجري نسائي راقي - 9 قطع متنوعة",
+        price: 4500, // السعر بالدينار (مثال: 450 ألف)
+        originalPrice: 6000,
+        image: "https://raw.githubusercontent.com/alizedam9721/zdmstore/main/images/lingerie.jpg",
+        description: "مجموعة حصرية من 9 قطع لانجري بتصميمات شبكية جذابة ومختلفة، مرنة وتناسب جميع المقاسات بجودة ممتازة."
     }
 ];
