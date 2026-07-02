de9f7f479d2ea4823e5d5dcf90546fc76f1786ba
alizedam9721-dot <alizedam9721@gmail.com>
2026-07-02 22:19:51 +0100

    Update products.js

--- products.js
+++ products.js
@@ -1,26 +1,17 @@
-const products = [
+var products = [
     {
-        id: 1,
         name: "مجموعة أساور ذهبية راقية (Acier Inoxydable)",
-        price: 2500, // السعر بالدينار الجزائري (مثال: 250 ألف)
-        originalPrice: 3500,
-        image: "https://raw.githubusercontent.com/alizedam9721/zdmstore/main/images/bracelets.jpg",
-        description: "مجموعة أساور (أنورب) من الفولاذ المقاوم للصدأ بجودة عالية، تصميم عصري ولامع يناسب كل المناسبات."
+        price: "2500 دج",
+        image: "images/Bracelets.jpg.heic"
     },
     {
-        id: 2,
         name: "أحمر شفاه كبسولة 12 لون Mini Bag",
-        price: 1800, // السعر بالدينار (مثال: 180 ألف)
-        originalPrice: 2400,
-        image: "https://raw.githubusercontent.com/alizedam9721/zdmstore/main/images/lipstick.jpg",
-        description: "حقيبة صغيرة تحتوي على 12 لون مميز من أحمر الشفاه على شكل كبسولات مات وناعمة، سهلة الحمل في الحقيبة اليومية."
+        price: "1800 دج",
+        image: "images/lipstick.jpg.jpg"
     },
     {
-        id: 3,
         name: "طقم لانجري نسائي راقي - 9 قطع متنوعة",
-        price: 4500, // السعر بالدينار (مثال: 450 ألف)
-        originalPrice: 6000,
-        image: "https://raw.githubusercontent.com/alizedam9721/zdmstore/main/images/lingerie.jpg",
-        description: "مجموعة حصرية من 9 قطع لانجري بتصميمات شبكية جذابة ومختلفة، مرنة وتناسب جميع المقاسات بجودة ممتازة."
+        price: "4500 دج",
+        image: "images/lingerie1.jpg.jpg"
     }
 ];
